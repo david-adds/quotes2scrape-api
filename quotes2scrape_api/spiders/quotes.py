967c29cf38b36ab64bdaf2a5a3b8249fe03e7b07
david-adds <55636118+david-adds@users.noreply.github.com>
2022-01-03 02:15:43 -0300

    scrape author, tags and quote from 1st page

--- quotes2scrape_api/spiders/quotes.py
+++ quotes2scrape_api/spiders/quotes.py
@@ -1,4 +1,5 @@
 import scrapy
+import json
 
 
 class QuotesSpider(scrapy.Spider):
@@ -7,4 +8,11 @@ class QuotesSpider(scrapy.Spider):
     start_urls = ['https://quotes.toscrape.com/api/quotes?page=1']
 
     def parse(self, response):
-        print(response.body)
+        resp = json.loads(response.body)
+        quotes = resp.get('quotes')
+        for quote in quotes:
+            yield {
+                'author': quote.get('author').get('name'),
+                'tags': quote.get('tags'),
+                'quote_text': quote.get('text')
+            }
